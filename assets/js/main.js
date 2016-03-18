3b28980d3fdcf549b34cd9f14c4f37b188a2b672
Ivan Vrouwe <ivan.donald.vrouwe@gmail.com>
2016-03-18 15:37:43 -0600

    Switched "Resume"'s permalink button from "View This Document" to "Print This Document" for browsers with access to the JS; within the permalink, added the "Print" anchor via JS so that non-JS-enabled users (people who clicked "View", not "Print") aren't stuck with a non-functioning "Print" anchor.
    To do:
    
    - Fix a bug I found:
    
    I need to refactor JS logic (switches, conditionals) so that users
    accessing the permalink with no “#print” hash appended to it can click
    to print, but without launching a new browser window.
    
    Additionally, I should be commenting and clarifying throughout my JS as
    I refactor.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,80 @@
 
 	"use strict";
 
-	var documentToolbar = document.querySelector(".document .toolbar");
+	var documentToolbar = document.querySelector(".document .toolbar"),
+		viewPrintAnchor,
+		index;
+
+	function appendPrintTextAndSpanToAnchorAndAddHash () {
+		switch (window.location.hash === "#print") {
+			case false:
+				(function () {
+					if (viewPrintAnchor.classList.contains("view-or-print-anchor") === true) {
+						var printAnchorText1 = document.createTextNode("Print This Document (Permalink"),
+							printAnchorSpan = document.createElement("span"),
+							printAnchorSpanText = document.createTextNode(" - launches in new window"),
+							printAnchorText2 = document.createTextNode(")"),
+							nodesToAppend = [];
+
+						viewPrintAnchor.href = viewPrintAnchor.href + "#print";
+						
+						printAnchorSpan.classList.add("visually-hidden");
+						printAnchorSpan.appendChild(printAnchorSpanText);
+
+						nodesToAppend = [printAnchorText1, printAnchorSpan, printAnchorText2];
+
+						for (index = 0; index < nodesToAppend.length; index++) {
+							viewPrintAnchor.appendChild(nodesToAppend[index]);
+						}
+					}
+				}());
+				break;
+			case true:
+				(function () {
+					var printAnchorText = document.createTextNode("Print This Document");
+
+					viewPrintAnchor.appendChild(printAnchorText);
+				}());
+		}
+
+		viewPrintAnchor.addEventListener("click", launchPrintDialog);
+	}
 
 	function launchPrintDialog () {
-		window.print();
+		setTimeout(function () {
+			if (window.location.hash === "#print") {
+				window.print();
+			}
+		}, 200);
 	}
 
-	if (documentToolbar.children.length < 3) {
-		var listItem = document.createElement("li"),
-			printButton = document.createElement("button"),
-			printButtonText = document.createTextNode("Print This Document");
+	// Checks for document on index page ("if") v. document on its own permalink page ("else if")
+	(function () {
+		viewPrintAnchor = documentToolbar.querySelector(".view-or-print-anchor");
 
-			printButton.classList.add("launches-print-dialog");
+		if (viewPrintAnchor && window.location.hash === "") {
+			while (viewPrintAnchor.childNodes.length > 0) {
+				viewPrintAnchor.removeChild(viewPrintAnchor.childNodes[0]);
+			}
+				appendPrintTextAndSpanToAnchorAndAddHash();
 
-			printButton.appendChild(printButtonText);
+		} else if (!viewPrintAnchor) {
+			var listItem = document.createElement("li"),
+				anchor = document.createElement("a");
 
-			listItem.appendChild(printButton);
+				anchor.classList.add("view-or-print-anchor");
+				anchor.href="http://addictedtocoding.com/assets/documents/resume-ivan-vrouwe.html#print";
+				listItem.appendChild(anchor);
+				documentToolbar.insertBefore(listItem, documentToolbar.children[0]);
 
-			documentToolbar.insertBefore(listItem, documentToolbar.children[0]);
+				viewPrintAnchor = documentToolbar.querySelector(".view-or-print-anchor");
 
-			printButton = documentToolbar.querySelector(".launches-print-dialog");
+				appendPrintTextAndSpanToAnchorAndAddHash();
 
-			printButton.addEventListener("click", launchPrintDialog);
-	}
+				viewPrintAnchor.classList.remove("view-or-print-anchor");
+		}
+	}());
+
 
 	if (window.location.hash === "#print") {
 		window.scroll(0, document.body.scrollHeight);
